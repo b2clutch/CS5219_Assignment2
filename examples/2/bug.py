bc89522c44e573c43fc05d95819cfe8e4cbf5f30
b2clutch <63180028+b2clutch@users.noreply.github.com>
2023-11-20 22:07:02 +0800

    Delete examples directory

--- examples/2/bug.py
+++
@@ -1,8 +0,0 @@
-def get_initial_corpus():
-    return ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]
-
-def entrypoint(s):
-    if s == "areallyreallyreallyreallyreallyreallylongstring":
-        print("Found the bug!")
-        exit(219)
-
